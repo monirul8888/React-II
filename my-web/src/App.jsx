28091925da31b2167455952e7408802018b777e6
monirul8888 <monirul429588@gmail.com>
2026-03-04 22:14:31 +0600

    Added UseEffect

---
+++ my-web/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Navbar from './Components/Navbar/Navbar'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar></Navbar>
+     <Blogs></Blogs>
+    
+    </>
+  )
+}
+
+export default App
