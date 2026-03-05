e5bc794c6fe324527ce3c6e68230cc160a995ca0
monirul8888 <monirul429588@gmail.com>
2026-03-06 04:49:26 +0600

    Added my-web-ii

---
+++ my-web-ii/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Hero from "./Navbar/Hero";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  return <>
+  <Navbar></Navbar>
+  <Hero></Hero>
+  </>;
+}
+
+export default App;
