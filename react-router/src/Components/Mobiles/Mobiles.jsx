fe2aa7e62fc6d12726166b52d6165d40c2e98a3e
monirul8888 <monirul429588@gmail.com>
2026-03-06 15:27:41 +0600

    Added Router

---
+++ react-router/src/Components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobiles = () => {
+    return (
+        <div>
+            <h3>This is Mobile Page</h3>
+        </div>
+    );
+};
+
+export default Mobiles;
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,17 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Root from "./Components/Root/Root.jsx"
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Home from './Components/Home/Home.jsx'
+import Mobiles from './Components/Mobiles/Mobiles.jsx'
+import Laptops from './Components/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello World</div>,
+    path : "/",
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: "/mobiles", Component: Mobiles},
+      {path: "/laptops", Component: Laptops}
+    ]
+    
   },
-]);
+  {
+    path: "/about",
+    element : <div>
+      <h3>Green University of Bangladesh</h3>
+    </div>
+  },
+  {
+    path: "/app",
+    Component: App
+  }
+])
+
+
+
 
 
 
