3e1bb0850e41c8f288b530a8e9072da660033999
monirul8888 <monirul429588@gmail.com>
2026-03-07 15:21:27 +0600

    Added Form Target

--- my-form/src/Components/SimpleForm/SimpleForm.jsx
+++ my-form/src/Components/SimpleForm/SimpleForm.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const SimpleForm = () => {
-    const handleSubmit = () =>{
+    const handleSubmit = (e) =>{
+        e.preventDefault();
         console.log("Form Submitted");
+        console.log(e.target.abir.value);
     }
     return (
         <div>
             <form onSubmit={handleSubmit} action="">
-                <input type="text" name="" id="" placeholder='Enter Your Name'/>
+                <input type="text" name="abir" id="" placeholder='Enter Your Name'/>
+                <br />
                 <input  type="submit" name="" id="" />
             </form>
         </div>
