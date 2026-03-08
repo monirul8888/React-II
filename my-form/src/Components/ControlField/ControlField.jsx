cc629c61d04f5f0ddbe0c2599ebd43b86a2f754d
monirul8888 <monirul429588@gmail.com>
2026-03-08 23:18:37 +0600

    Added Name Email

--- my-form/src/Components/ControlField/ControlField.jsx
+++ my-form/src/Components/ControlField/ControlField.jsx
@@ -4,18 +4,18 @@ import { useState } from 'react';
 const ControlField = () => {
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log(e.target.email.value);
+        console.log(email)
     }
     const [password, setPassword] = useState("")
     const [error, setError] =useState("");
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
     const handleNameOnChange = (e) => {
-        console.log(e.target.value);
+        setName(e.target.value);
     }
 
     const handlePasswordOnChange = (e) =>{
-        console.log(e.target.value);
         setPassword(e.target.value);
         if (password.length < 6){
             console.log("Password Must be 6 Char or Longer")
@@ -26,12 +26,17 @@ const ControlField = () => {
         }
     }
 
+    const handleOnChangeEmail = (e) =>{
+        setEmail(e.target.value);
+    }
+
 
     return (
         <div>
             <form onSubmit={handleSubmit} >
                
-                <input type="email" name="email" id="" required placeholder='Email'/>
+                <input type="email" name="email" id="" required placeholder='Email'
+                onChange={handleOnChangeEmail}/>
                 <br />
                 <input type="password" name="password" id="" placeholder='Password' defaultValue={password} onChange={handlePasswordOnChange} />
                 
